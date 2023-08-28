89365df96ae2cf861709dc49191bf118bd76e799
Amol Khamkar <Amol Khamkar@DESKTOP-15SGT2T>
2023-08-28 00:01:34 -0700

    [AmolKhamkar] added new flipkart functionality

--- src/flipkart.java
+++ src/flipkart.java
@@ -3,14 +3,13 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class amazon {
+public class flipkart {
 
 	public static void main(String[] args) throws InterruptedException {
 
 		
 		WebDriver driver= new ChromeDriver();
-		driver.get("https://www.flipkart.in/");
-		Thread.sleep(2000);
+		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		Thread.sleep(4000);
